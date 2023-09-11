7c533fc3f61bc45f5c0c8389883005aef68d898b
ifonsecaz <47679597+ifonsecaz@users.noreply.github.com>
2023-09-11 08:59:48 -0600

    Update detecRastreoSim.cpp

--- AutoModelCar/detecRastreoSim.cpp
+++ AutoModelCar/detecRastreoSim.cpp
@@ -236,6 +236,7 @@ void detecRegion(CascadeClassifier carC, Mat img, int cont, int x, int y, int wi
         imgGRAY = img;
     }
 
+if(x>0&&x<640&&y>0&&y<480&&width>0&&height>0){
     int x1, y1,width1,height1;
     int extra = (int)0.8 * width;
     if ((x + width +extra)> 640) {
@@ -367,7 +368,12 @@ void detecRegion(CascadeClassifier carC, Mat img, int cont, int x, int y, int wi
 		//Devuelve la imagen original si no hubo detecciones
         *imgF = imgGRAY;
     }
-
+else {
+		//Devuelve la imagen original si no hubo detecciones
+	String a = "/home/israel/Documents/k/test" + std::to_string(cont) + ".jpg";
+        *imgF = imgGRAY;
+        imwrite(a, imgGRAY);	
+    }
 }
 
 /*
